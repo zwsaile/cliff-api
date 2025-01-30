93d7e205ec16cf39bfeb809d5b53d135f07f9cac
Zachary Saile <zwsaile@gmail.com>
2025-01-30 12:42:00 -0800

    Fix deployment set up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 var cors = require('cors');
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/photos', express.static(path.join(__dirname, 'photos')));
 
 app.set('port', process.env.PORT || 8080);
 app.locals.title = 'Best Cliff Jumping Spots In America';
@@ -346,8 +349,6 @@ app.post('/api/v1/cliffs', (request, response) => {
   response.status(201).json({ id, name, type, maxHeight, depth, latLong, photo });
 });
 
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = (req, res) => {
+  app(req, res);
+};
