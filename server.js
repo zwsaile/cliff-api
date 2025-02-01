eeced77d90e4649decd6bcffa96de5187d428cba
Zachary Saile <zwsaile@gmail.com>
2025-01-31 17:15:30 -0800

    Adjust for hosting with Render

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -346,6 +346,14 @@ app.post('/api/v1/cliffs', (request, response) => {
   response.status(201).json({ id, name, type, maxHeight, depth, latLong, photo });
 });
 
-module.exports = (req, res) => {
-  app(req, res);
-};
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
